b9e0593b5aa05516c7d6cdab23bea68658288bc1
Roman Gordiienko <rom4uk691@gmail.com>
2024-04-30 11:18:43 +0300

    Finished

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,22 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const userName = useRef();
+  const [enteredName, setUserName] = useState("");
+
+  function handleClick() {
+    setUserName(userName.current.value);
+    userName.current.value = "";
+    // we litle bit violating rules of declarative writiing for React
+    //  and writting imperative cod
+    // straightly changing DOM, instead let it for React
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredName ? enteredName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={userName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
